e48fc68a9bfd806fc8d903e57116f8a61fe79c05
ongaeshi <ongaeshi0621@gmail.com>
2011-09-14 12:01:32 +0900

    [end] LESSON17
    サムネイルクリックで画像を切り替え
    画像の上でマウスを動かすとテキストが変わる
    画像のドラッグ＆ドロップ
    
    マウスイベント種類
      click
      dblclick
      mousedown
      mousemove
      mouseout
      mouseover
      mouseup
    
    キーイベント種類
      keydown
      keypress
    
    フォーム
      blur
      change
      focus
      select
      submit
    
    その他
      load
      resize
      scroll
      unload
      error

--- lesson17/event.js
+++ lesson17/event.js
@@ -6,5 +6,36 @@ $(function() {
   $('#list img').click(function() {
     $('#big').attr('src', $(this).attr('src'));    
   });
+
+  // 画像の上でマウスを動かしてみてね
+  $('img#hamster').mousemove(function(e) {
+    var x = e.pageX;
+    if (x < 175) {
+      $('#msg').text('こんにちは');
+    } else if (x < 410) {
+      $('#msg').text('ガジガジ');
+    } else {
+      $('#msg').text('のど乾いたな');
+    }
+  });
+
+  // ドラッグ＆ドロップ
+  var flag = false;
+  $('img#dragdrop')
+    .mousedown(function() {
+      flag = true;
+      return false; // falseを返すとここでイベント終了(ブラウザ規定の挙動に移行しない)
+    })
+    .mouseup(function () {
+      flag = false;
+    })
+    .mousemove(function(e) {
+      if (flag) {
+        $(this).css({
+          top: e.pageY - 30,
+          left: e.pageX - 70
+        });
+      }
+    });
 });
 
