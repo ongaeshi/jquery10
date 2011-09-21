afbbd0295b22e221b089aa348509204e1f2bd74c
ongaeshi <ongaeshi0621@gmail.com>
2011-09-21 23:33:54 +0900

    [end] LESSON32
    ページング処理を追加
    isNotANumber(v) // isNaNだと、引数が""の時にfalseが返ってしまうので専用関数を作る
    var page_cnt = Math.ceil($('ResultSet', data).attr('totalResultsAvailable') / 20); // page数を計算
    $('.pager_link').live('click', onsearch); // 新しく.pager_linkを作った時もメソッドを追加
    大きなコードを読むときは、詳細なコードに踏み込むのではなく、ブロック単位にコードを把握していく。
    .attr('href', '#') // <a href="#"> 的なやつ(ダミーリンク)

--- lesson32/public/yahoo.js
+++ lesson32/public/yahoo.js
@@ -1,14 +1,23 @@
 $(function () {
+  // isNaN‚¾‚ÆAˆø”‚ª""‚Ì‚Éfalse‚ª•Ô‚Á‚Ä‚µ‚Ü‚¤‚Ì‚Åê—pŠÖ”‚ğì‚é
+  var isNotANumber = function(v) {
+    return isNaN(v) || v === "";
+  };
+
   var onsearch = function (e) {
+    var page_num = isNotANumber($(this).text()) ? 1 : $(this).text();
+    
     $.get(
       '/',
       {
         keywd: $('#keywd').val(),
-        page: 1
+        page: page_num
       },
       function (data) {
         $('#result').empty();
+        $('#pager').empty();
 
+        // video
         $('Result', data).each(function() {
           $('#result').append(
             $('<a></a>')
@@ -23,9 +32,26 @@ $(function () {
               )
           );
         });
-      }
-    );
+
+        // paging
+        var pager = $('#pager');
+        var page_cnt = Math.ceil($('ResultSet', data).attr('totalResultsAvailable') / 20);
+
+        if (page_cnt > 1) {
+          for (var i = 1; i <= page_cnt; i++) {
+            if (i > 10) { break; }
+            pager.append(
+              $('<a></a>')
+                .addClass('pager_link')
+                .attr('href', '#')
+                .text(i)
+            );
+          }
+        }
+      });
+    e.preventDefault();
   };
 
   $('#search').click(onsearch);
+  $('.pager_link').live('click', onsearch);
 });
