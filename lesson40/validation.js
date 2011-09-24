127d18b39810fb8a08e12b94005e1b61b6c2c74a
ongaeshi <ongaeshi0621@gmail.com>
2011-09-24 18:49:11 +0900

    [end] LESSON40
    プラグインは自分で作ることも出来る。
    LESSON22で作成した検証機能をプラグインとして書き換えてみる。
    (function ($) { ... } (jQuery); -> $()でもjQuery()でも呼び出せるようにするためのおまじない
    $.fn.extend({ ...});            -> jQueryに新たなメソッドを追加するためのメソッド
    処理の対象はthisキーワードで表す
    戻り値として、jQueryオブジェクトを返すのが基本 (メソッドチェーンを切らないようにしよう)

--- lesson40/validation.js
+++ lesson40/validation.js
@@ -5,70 +5,76 @@
 // range    
 // inarray 
 
-$(function () {
-  var msgs;
+(function ($){
+  $.fn.extend({
+    validate: function() {
+      var msgs;
 
-  var setError = function (elem, msg) {
-    msgs.push('<li>' + msg + '</li>');
-    $(elem)
-      .addClass('error_field')
-      .after('<span class="error_mark">*</span>');
-  };
+      var setError = function (elem, msg) {
+        msgs.push('<li>' + msg + '</li>');
+        $(elem)
+          .addClass('error_field')
+          .after('<span class="error_mark">*</span>');
+      };
 
-  $('#fm').submit(function (e) {
-    msgs = [];
-    $('error_mark', this).remove();
+      this.submit(function (e) {
+        msgs = [];
+        $('error_mark', this).remove();
 
-    $('.valid', this)           // thisを基点に検索
-      .removeClass('.error_field')
-      .filter('.required')
-      .each(function () {
-        if ($(this).val() === '') {
-          setError(this, $(this).prev('label').text() + 'は必須入力です。');
-        }
-      })
-      .end()
-      .filter('.length')
-      .each(function () {
-        if ($(this).val().length > $(this).data('length')) {
-          setError(this, $(this).prev('label').text() + 'は' + $(this).data('length') + '文字以内で入力して下さい。');
-        }
-      })
-      .end()
-      .filter('.range')
-      .each(function () {
-        var v = parseFloat($(this).val());
-        if (v < $(this).data('min') || v > $(this).data('max')) {
-          setError(this, $(this).prev('label').text() + 'は' + $(this).data('min') + '～' + $(this).data('max') + 'の範囲で入力して下さい。');
-        }
-      })
-      .end()
-      .filter('.inarray')
-      .each(function () {
-        var opts = $(this).data('option').split(' ');
+        $('.valid', this)           // thisを基点に検索
+          .removeClass('.error_field')
+          .filter('.required')
+          .each(function () {
+            if ($(this).val() === '') {
+              setError(this, $(this).prev('label').text() + 'は必須入力です。');
+            }
+          })
+            .end()
+          .filter('.length')
+          .each(function () {
+            if ($(this).val().length > $(this).data('length')) {
+              setError(this, $(this).prev('label').text() + 'は' + $(this).data('length') + '文字以内で入力して下さい。');
+            }
+          })
+            .end()
+          .filter('.range')
+          .each(function () {
+            var v = parseFloat($(this).val());
+            if (v < $(this).data('min') || v > $(this).data('max')) {
+              setError(this, $(this).prev('label').text() + 'は' + $(this).data('min') + '～' + $(this).data('max') + 'の範囲で入力して下さい。');
+            }
+          })
+            .end()
+          .filter('.inarray')
+          .each(function () {
+            var opts = $(this).data('option').split(' ');
 
-        if ($.inArray($(this).val(), opts) === -1) {
-          setError(this, $(this).prev('label').text() + 'は [' + opts.toString() + '] のいずれかで入力して下さい。');
-        }
-      })
-      .end()
-      .filter('.regexp')
-      .each(function () {
-        var reg = new RegExp($(this).data('pattern'), 'gi');
-        if (!reg.test($(this).val())) {
-          setError(this, $(this).prev('label').text() + 'の形式(' + $(this).data('pattern') + ')が間違っています。');
-        }
-      })
-    ;
+            if ($.inArray($(this).val(), opts) === -1) {
+              setError(this, $(this).prev('label').text() + 'は [' + opts.toString() + '] のいずれかで入力して下さい。');
+            }
+          })
+            .end()
+          .filter('.regexp')
+          .each(function () {
+            var reg = new RegExp($(this).data('pattern'), 'gi');
+            if (!reg.test($(this).val())) {
+              setError(this, $(this).prev('label').text() + 'の形式(' + $(this).data('pattern') + ')が間違っています。');
+            }
+          })
+            ;
 
-    // エラーメッセージ処理
-    if (msgs.length === 0) {
-      $('#error_summary').css('display', 'none');
-    } else {
-      $('#error_summary')
-        .css('display', 'block')
-        .html(msgs.join(''));
-      e.preventDefault();
+        // エラーメッセージ処理
+        if (msgs.length === 0) {
+          $('#error_summary').css('display', 'none');
+        } else {
+          $('#error_summary')
+            .css('display', 'block')
+            .html(msgs.join(''));
+          e.preventDefault();
+        }
+      });
+
+      return this;
     }
   });
-});
+}) (jQuery);
