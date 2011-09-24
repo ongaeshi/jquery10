8e0d79eae0e5c3f0b02d56b557b9d1239c75cca1
ongaeshi <ongaeshi0621@gmail.com>
2011-09-24 17:48:46 +0900

    [start] LESSON38 Zoomboxプラグインで動画をズーム表示しよう
    lesson33からコピー

---
+++ lesson38/app.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+require 'rubygems'
+require 'sinatra'
+require 'open-uri'
+require 'erb'
+
+# reloader
+require "sinatra/base"
+require "sinatra/reloader" if development?
+
+NUM = 20                        # 1ページの表示数
+  
+before do
+  content_type 'xml' # 常にContent-Typeを 'application/xml'に
+end
+
+get '/' do
+  p = {
+    :appid   => 'gFLs9RCxg65wBLXYLsx8gELmJHpYa4dq8cg1bfxalTiYgOL7u0jPeW4VrmfY6H.Zvyqh',
+    :query   => escape(params[:keywd]),
+    :start   => (params[:page].to_i - 1) * NUM + 1,
+    :results => NUM,
+  }
+  # puts p
+
+  q = p.map{|k,v| "#{k}=#{v}"}.join('&')
+  open("http://search.yahooapis.jp/VideoSearchService/V2/videoSearch?#{q}")
+
+  # ajaxErrorテストをするにはここをコメントアウト (エラーメッセージ：Internal Server Error)
+  # raise
+end
+
---
+++ lesson38/public/yahoo.js
@@ -0,0 +1,71 @@
+﻿$(function () {
+  $('#prog')
+    .ajaxStart(function() { $(this).show(); })
+    .ajaxStop( function() { $(this).hide(); })
+  ;
+
+  $('#result')
+    .ajaxError(function(e, xhr, opts, err) {
+      $(this).html('<strong>エラーメッセージ：' + err + '</strong>');
+    });
+
+  $.ajaxSetup({
+    cache: false
+  });
+  
+  // isNaNだと、引数が""の時にfalseが返ってしまうので専用関数を作る
+  var isNotANumber = function(v) {
+    return isNaN(v) || v === "";
+  };
+
+  var onsearch = function (e) {
+    var page_num = isNotANumber($(this).text()) ? 1 : $(this).text();
+    
+    $.get(
+      '/',
+      {
+        keywd: $('#keywd').val(),
+        page: page_num
+      },
+      function (data) {
+        $('#result').empty();
+        $('#pager').empty();
+
+        // video
+        $('Result', data).each(function() {
+          $('#result').append(
+            $('<a></a>')
+              .attr('href', $('> Url', this).text())
+              .append(
+                $('<img>')
+                  .addClass('thumb')
+                  .attr({
+                    src: $('Thumbnail Url', this).text(),
+                    title: $('Summary', this).text()
+                  })
+              )
+          );
+        });
+
+        // paging
+        var pager = $('#pager');
+        var page_cnt = Math.ceil($('ResultSet', data).attr('totalResultsAvailable') / 20);
+
+        if (page_cnt > 1) {
+          for (var i = 1; i <= page_cnt; i++) {
+            if (i > 10) { break; }
+            pager.append(
+              $('<a></a>')
+                .addClass('pager_link')
+                .attr('href', '#')
+                .text(i)
+            );
+          }
+        }
+      });
+    e.preventDefault();
+  };
+
+  $('#search').click(onsearch);
+  $('.pager_link').live('click', onsearch);
+});
